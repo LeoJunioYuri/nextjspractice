7515e73c49bf14e1f9365638227f3602f1756582
leojunioyuri <leojunioyuri@hotmail.com>
2024-03-12 15:09:00 -0300

    feat: optimizing page loading with skeleton

--- nextjs-dashboard/app/dashboard/(overview)/page.tsx
+++ nextjs-dashboard/app/dashboard/(overview)/page.tsx
@@ -1,7 +1,7 @@
 import { Card } from '@/app/ui/dashboard/cards';
 import RevenueChart from '@/app/ui/dashboard/revenue-chart';
 import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
-import { fetchRevenue, fetchLatestInvoices, fetchCardData } from '../lib/data';
+import { fetchRevenue, fetchLatestInvoices, fetchCardData } from '../../lib/data';
 import { lusitana } from '@/app/ui/fonts';
 
 export default async function Page() {
