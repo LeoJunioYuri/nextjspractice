9888d71a830affe5fbb2d50d2ef714671da1f434
leojunioyuri <leojunioyuri@hotmail.com>
2024-04-01 11:56:45 -0300

    feat: using debouncing for search delay

--- nextjs-dashboard/app/ui/search.tsx
+++ nextjs-dashboard/app/ui/search.tsx
@@ -2,12 +2,15 @@
 
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
 import { useSearchParams, usePathname, useRouter } from 'next/navigation';
+import { useDebouncedCallback } from 'use-debounce';
 
 export default function Search({ placeholder }: { placeholder: string }) {
   const searchParams = useSearchParams();
   const pathname = usePathname();
   const { replace } = useRouter();
-  function handleSearch(term: string) {
+
+  const handleSearch = useDebouncedCallback((term) => {
+      console.log(`Searching... ${term}`); //watch search delay
       const params = new URLSearchParams(searchParams);
       if (term) {
         params.set('query', term);
@@ -15,7 +18,8 @@ export default function Search({ placeholder }: { placeholder: string }) {
         params.delete('query');
       }
       replace(`${pathname}?${params.toString()}`);
-  }
+  }, 300);
+
   return (
     <div className="relative flex flex-1 flex-shrink-0">
       <label htmlFor="search" className="sr-only">
