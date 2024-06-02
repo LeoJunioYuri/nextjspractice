6a503aa3edbf635dc48095012a1c156d2eb69f0f
leojunioyuri <leojunioyuri@hotmail.com>
2024-06-02 01:37:35 -0300

    fix: search.tsx

--- nextjs-dashboard/app/ui/search.tsx
+++ nextjs-dashboard/app/ui/search.tsx
@@ -1,24 +1,23 @@
 'use client';
-
+ 
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
-import { useSearchParams, usePathname, useRouter } from 'next/navigation';
+import { usePathname, useRouter, useSearchParams } from 'next/navigation';
 import { useDebouncedCallback } from 'use-debounce';
-
+ 
 export default function Search({ placeholder }: { placeholder: string }) {
   const searchParams = useSearchParams();
-  const pathname = usePathname();
   const { replace } = useRouter();
-
+  const pathname = usePathname();
+ 
   const handleSearch = useDebouncedCallback((term) => {
-      // console.log(`Searching... ${term}`); //watch search delay
-      params.set('page', '1');
-      const params = new URLSearchParams(searchParams);
-      if (term) {
-        params.set('query', term);
-      } else {
-        params.delete('query');
-      }
-      replace(`${pathname}?${params.toString()}`);
+    const params = new URLSearchParams(searchParams);
+    params.set('page', '1');
+    if (term) {
+      params.set('query', term);
+    } else {
+      params.delete('query');
+    }
+    replace(`${pathname}?${params.toString()}`);
   }, 300);
 
   return (
