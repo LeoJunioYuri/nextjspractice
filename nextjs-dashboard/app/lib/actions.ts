95c6023e180e948c3bfcde5c8afe1e0b70f33a52
leojunioyuri <leojunioyuri@hotmail.com>
2024-06-10 22:53:28 -0300

    feat: inserting the data into the database

--- nextjs-dashboard/app/lib/actions.ts
+++ nextjs-dashboard/app/lib/actions.ts
@@ -1,6 +1,7 @@
 'use server';
  
 import { z } from 'zod';
+import { sql } from '@vercel/postgres';
  
 const FormSchema = z.object({
   id: z.string(),
@@ -20,4 +21,9 @@ export async function createInvoice(formData: FormData) {
   });
   const amountInCents = amount * 100;
   const date = new Date().toISOString().split('T')[0];
+
+  await sql`
+  INSERT INTO invoices (customer_id, amount, status, date)
+  VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+`;
 }
